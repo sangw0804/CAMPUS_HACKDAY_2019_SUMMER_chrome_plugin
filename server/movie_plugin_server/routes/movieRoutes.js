8fbd1fb0e32b27fe65746a5af2cdb8e59698a6de
SangWoo <sangw0804@likelion.org>
2019-05-16 22:17:10 +0900

    mongoose 추가

--- server/movie_plugin_server/routes/movieRoutes.js
+++ server/movie_plugin_server/routes/movieRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const axios = require('axios');
+
 const config = require('../config');
 const router = express.Router();
 const { User } = require('../models/user');
